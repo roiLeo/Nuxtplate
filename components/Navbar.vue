5a80f39e097568ec7466462867ae9c5d9c23e70b
roiLeo <medina.leo42@gmail.com>
2022-06-16 15:57:02 +0200

    🌞/🌚 mode

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="bg-black text-white p-8">
-		<nav
-			class="
-				flex
-				items-center
-				justify-between
-				relative
-				mx-auto
-				max-w-5xl
-				w-full
-			">
-			<h1 class="dark:text-green-500">Hello Nuxt!</h1>
-			<ColorModeButton class="hover:text-gray-400" />
-		</nav>
-	</div>
+  <div class="bg-black text-white p-8">
+    <nav
+      class="
+        flex
+        items-center
+        justify-between
+        relative
+        mx-auto
+        max-w-5xl
+        w-full
+      ">
+      <h1 class="dark:text-green-500">Hello Nuxt!</h1>
+      <ColorModeButton class="hover:text-gray-400" />
+    </nav>
+  </div>
 </template>
