f486b90569ea23593d3b891f74cc02900375e8c0
roiLeo <medina.leo42@gmail.com>
2023-03-01 14:47:32 +0100

    ⬆️ upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-	buildModules: [
-		'nuxt-windicss'
-	],
-	css: [
-		'virtual:windi.css'
-	]
+  modules: ['nuxt-windicss'],
 })
